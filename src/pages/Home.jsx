f3dab64231539dfa0e91b5eaed83454bd8c0065f
Loup Aubour <loup.aubour@gmail.com>
2024-01-15 16:43:08 +0100

    Basic island rotation feature done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense,useState } from "react";
 import Loader from "../components/Loader";
 
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 import Sky from "../models/Sky";
 import Island from "../models/Island";
 
 const Home = () => {
+	const [isRotating, setIsRotating] = useState(false);
+
 	const adjustIslandForScreensSize = () => {
 		let screenScale = null;
 		let screenPosition = [0, -6.5, -43];
@@ -21,12 +25,28 @@ const Home = () => {
 		return [screenScale, screenPosition, rotation]
 	}
 
+	const adjustPlaneForScreensSize = () => {
+		let screenScale, screenPosition;
+
+		if(window.innerWidth < 768) {
+			screenScale = [1.5, 1.5, 1.5];
+			screenPosition = [0, -1.5, 0];
+		}
+		else {
+			screenScale = [3, 3, 3];
+			screenPosition = [0, -4, -4];
+		}
+
+		return [screenScale, screenPosition]
+	}
+
 	const[islandScale, islandPosition, islandRotation] = adjustIslandForScreensSize();
+	const[planeScale, planePosition] = adjustPlaneForScreensSize();
 
 	return (
 		<section className="w-full h-screen relative">
 			<Canvas
-				className="w-full h-screen relative"
+				className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`}
 				camera={{ near: 0.1, far: 1000 }}>
 
 				<Suspense fallback={<Loader/>}>
@@ -34,12 +54,20 @@ const Home = () => {
 					<ambientLight intensity={0.5}/>
 					<hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
 
+					<Bird/>
 					<Sky/>
 					<Island
 						position={islandPosition}
 						scale={islandScale}
 						rotation={islandRotation}
+						isRotating={isRotating}
+						setIsRotating={setIsRotating}
 					/>
+					<Plane
+					isRotating={isRotating}
+					planeScale={planeScale}
+					planePosition={planePosition}
+					rotation={[0, 20, 0]}/>
 				</Suspense>
 			</Canvas>
 		</section>
