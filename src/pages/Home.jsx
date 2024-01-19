f931c8d08dfdd329c3f65478331aa3c670f05d65
Loup Aubour <loup.aubour@gmail.com>
2024-01-19 08:29:51 +0100

    Homepage popups render logic done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Bird from "../models/Bird";
 import Plane from "../models/Plane";
 import Sky from "../models/Sky";
 import Island from "../models/Island";
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
 	const [isRotating, setIsRotating] = useState(false);
@@ -16,47 +17,55 @@ const Home = () => {
 		let screenPosition = [0, -6.5, -43];
 		let rotation = [0.1, 4.7, 0];
 
-		if(window.innerWidth < 768) {
+		if (window.innerWidth < 768) {
 			screenScale = [0.9, 0.9, 0.9];
-		}
-		else {
+		} else {
 			screenScale = [1, 1, 1];
 		}
 
-		return [screenScale, screenPosition, rotation]
-	}
+		return [screenScale, screenPosition, rotation];
+	};
 
 	const adjustPlaneForScreensSize = () => {
 		let screenScale, screenPosition;
 
-		if(window.innerWidth < 768) {
+		if (window.innerWidth < 768) {
 			screenScale = [1.5, 1.5, 1.5];
 			screenPosition = [0, -1.5, 0];
-		}
-		else {
+		} else {
 			screenScale = [3, 3, 3];
 			screenPosition = [0, -4, -4];
 		}
 
-		return [screenScale, screenPosition]
-	}
+		return [screenScale, screenPosition];
+	};
 
-	const[islandScale, islandPosition, islandRotation] = adjustIslandForScreensSize();
-	const[planeScale, planePosition] = adjustPlaneForScreensSize();
+	const [islandScale, islandPosition, islandRotation] =
+		adjustIslandForScreensSize();
+	const [planeScale, planePosition] = adjustPlaneForScreensSize();
 
 	return (
 		<section className="w-full h-screen relative">
+			<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+				{currentStage && <HomeInfo currentStage={currentStage} />}
+			</div>
 			<Canvas
-				className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`}
-				camera={{ near: 0.1, far: 1000 }}>
+				className={`w-full h-screen bg-transparent ${
+					isRotating ? "cursor-grabbing" : "cursor-grab"
+				}`}
+				camera={{ near: 0.1, far: 1000 }}
+			>
+				<Suspense fallback={<Loader />}>
+					<directionalLight position={[10, 1, 1]} intensity={2} />
+					<ambientLight intensity={0.5} />
+					<hemisphereLight
+						skyColor="#b1e1ff"
+						groundColor="#000000"
+						intensity={1}
+					/>
 
-				<Suspense fallback={<Loader/>}>
-					<directionalLight position={[10, 1, 1]} intensity={2}/>
-					<ambientLight intensity={0.5}/>
-					<hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
-
-					<Bird/>
-					<Sky isRotating={isRotating}/>
+					<Bird />
+					<Sky isRotating={isRotating} />
 					<Island
 						position={islandPosition}
 						scale={islandScale}
@@ -66,10 +75,11 @@ const Home = () => {
 						setCurrentStage={setCurrentStage}
 					/>
 					<Plane
-					isRotating={isRotating}
-					planeScale={planeScale}
-					planePosition={planePosition}
-					rotation={[0, 20, 0]}/>
+						isRotating={isRotating}
+						planeScale={planeScale}
+						planePosition={planePosition}
+						rotation={[0, 20.1, 0]}
+					/>
 				</Suspense>
 			</Canvas>
 		</section>
